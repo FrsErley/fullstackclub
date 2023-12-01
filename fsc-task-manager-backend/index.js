e5df72156bef38d21365f32169665b4cd5c7f3a8
frsErley <frsleeh@gmail.com>
2023-12-01 16:53:01 -0300

    feat: add router file to task entity

--- fsc-task-manager-backend/index.js
+++ fsc-task-manager-backend/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+const TaskRouter = require("./src/routes/task.routes");
 const connectToDatabase = require("./src/database/mongoose.database");
-const TaskModel = require("./src/models/tsk.models");
 
 dotenv.config();
 const app = express();
@@ -10,44 +9,7 @@ app.use(express.json());
 
 connectToDatabase();
 
-app.get("/tasks", async (req, res) => {
-    try {
-        const tasks = await TaskModel.find({});
-        res.status(200).send(tasks);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.post("/tasks", async (req, res) => {
-    try {
-        const newTask = new TaskModel(req.body);
-
-        await newTask.save();
-
-        res.status(201).send(newTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-
-        const taskToDelete = await TaskModel.findById(taskId);
-
-        if (!taskToDelete) {
-            return res.status(500).send("Essa tarefa não foi encontrada.");
-        }
-
-        const deletedTask = await TaskModel.findByIdAndDelete(taskId);
-
-        res.status(200).send(deletedTask);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-});
+app.use("/tasks", TaskRouter);
 
 app.listen(8000, () => {
     console.log("Listening on port 8000!");
