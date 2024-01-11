6a51a6a4e5ce207105c2792a0b7471638da6cdb1
frsErley <frsleeh@gmail.com>
2024-01-11 14:16:20 -0300

    refactor: convert components to class

--- fsc-task-manager-frontend/src/App.js
+++ fsc-task-manager-frontend/src/App.js
@@ -1,28 +1,75 @@
-import {useState} from 'react';
-import TaskItem from './components/TaskItem';
-
-const App = () => {
   // eslint-disable-next-line no-unused-vars
-  const [tasks, setTasks] = useState([
-    {
-      id: '1',
-      description: 'Estudar Programação',
-      isCompleted: false,
-    },
-    {
-      id: '2',
-      description: 'Ler',
-      isCompleted: true,
+import React from 'react'
+import TaskItem from './components/TaskItem'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleStateChange = this.handleStateChange.bind(this);
+    this.state = {
+      tasks: [
+        {
+          id: '1',
+          description: 'Estudar Programação',
+          isCompleted: false,
+        },
+        {
+          id: '2',
+          description: 'Ler',
+          isCompleted: true,
+        }
+      ]
     }
-  ]);
-    
+  }
+
+  handleStateChange() {
+    this.setState({
+      tasks: [],
+    })
+  }
+
+  render() {
     return (
       <>
-        {tasks.map(task => (
+        {this.state.tasks.map(task => (
         <TaskItem key={task.id} task={task}/>
         ))}
+        <button onClick={this.handleStateChange}>Limpar tarefas</button>
       </>
-    );
+    )
+  }
 }
 
 export default App;
+
+
+
+
+
+// import {useState} from 'react';
+// import TaskItem from './components/TaskItem';
+
+// const App = () => {
+//   const [tasks, setTasks] = useState([
+    // {
+    //   id: '1',
+    //   description: 'Estudar Programação',
+    //   isCompleted: false,
+    // },
+    // {
+    //   id: '2',
+    //   description: 'Ler',
+    //   isCompleted: true,
+    // }
+//   ]);
+    
+//     return (
+      // <>
+      //   {tasks.map(task => (
+      //   <TaskItem key={task.id} task={task}/>
+      //   ))}
+      // </>
+//     );
+// }
+
+// export default App;
