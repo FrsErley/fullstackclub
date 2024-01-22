84dd756bba68d58411eeae1aa30a201a4d45a869
frsErley <frsleeh@gmail.com>
2024-01-22 14:30:16 -0300

    fix: CustomInput label

--- fsc-task-manager-frontend/src/components/CustomInput.jsx
+++ fsc-task-manager-frontend/src/components/CustomInput.jsx
@@ -1,16 +1,22 @@
-import "/TaskItem.scss"
+import "./CustomInput.scss"
 
-const CustomInput = ({label, value}) => {
+const CustomInput = ({label, value, onChange}) => {
     return (
         <div className="custom-input-container">
-            <input type="text" className="custom-input" />
+            <input 
+                type="text" 
+                className="custom-input"
+                onChange={(e) => onChange(e)} 
+                />
 
             {label ? (
                 <label 
                     className={`${
-                        value.lenght > 0 ? "shrink" : ""
+                        value.length > 0 ? "shrink" : ""
                     } custom-input-label`}
-                ></label>
+                >
+                    {label}
+                </label>
             ) : null}
         </div>
     );
