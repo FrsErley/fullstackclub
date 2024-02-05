e935b4830af510cb7a889efcbd629e73d009bd9a
frsErley <frsleeh@gmail.com>
2024-02-05 13:10:11 -0300

    feat: add function add task

--- fsc-task-manager-frontend/src/components/AddTask.jsx
+++ fsc-task-manager-frontend/src/components/AddTask.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { FaPlus } from 'react-icons/fa'
+import axios from 'axios'
+import {useAlert} from 'react-alert'
 
 import CustomInput from "./CustomInput"
 import CustomButton from './CustomButton';
@@ -9,8 +11,25 @@ import "./AddTask.scss"
 const AddTask = () => {
     const [task, setTask] = useState("");
 
+    const alert = useAlert();
+
     const onChange = (e) => {
         setTask(e.target.value);
+    };
+
+    const handleTaskAddition = async () => {
+        try {
+            if(task.length === 0) {
+                return alert.error('Tarefa precisa de uma descrição para ser adicionada')
+            }
+
+            await axios.post('http://localhost:8000/tasks', {
+                description: task,
+                isCompleted: false
+            })
+        } catch (error) {
+
+        }
     }
 
     return (
@@ -20,7 +39,7 @@ const AddTask = () => {
                 value={task} 
                 onChange={onChange}
             />
-            <CustomButton>
+            <CustomButton onClick={handleTaskAddition}>
                 <FaPlus size={14} color="#ffffff" />
             </CustomButton>
         </div>
