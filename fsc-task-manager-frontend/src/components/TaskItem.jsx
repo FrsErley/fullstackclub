6c15fc528251a10b61420359377784e7f246a569
frsErley <frsleeh@gmail.com>
2024-02-05 13:49:07 -0300

    feat: add function to change a task status

--- fsc-task-manager-frontend/src/components/TaskItem.jsx
+++ fsc-task-manager-frontend/src/components/TaskItem.jsx
@@ -17,6 +17,20 @@ const TaskItem = ({task, fetchTasks}) => {
             alert.error('Algo deu errado')
         }
     }
+
+    const handleTaskCompletionChange = async (e) => {
+        try {
+            await axios.patch(`http://localhost:8000/tasks/${task._id}`, {
+                isCompleted: e.target.checked,
+            })
+
+            await fetchTasks();
+
+            alert.success('A tarefa foi modificada com sucesso!')
+        } catch (error) {
+            alert.error('Algo deu errado')
+        }
+    }
     return (
         <div className="task-item-container">
             <div className="task-description">
@@ -27,7 +41,11 @@ const TaskItem = ({task, fetchTasks}) => {
                     }
                 >
                     {task.description}
-                    <input type="checkbox" defaultChecked={task.isCompleted} />
+                    <input 
+                        type="checkbox" 
+                        defaultChecked={task.isCompleted} 
+                        onChange={(e) => handleTaskCompletionChange(e)}
+                    />
                     <span 
                         className={
                             task.isCompleted 
