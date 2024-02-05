80cf7f54f1d26ac09a7a289157e2d53aa8914279
frsErley <frsleeh@gmail.com>
2024-02-05 15:39:31 -0300

    feat: add app container

--- fsc-task-manager-frontend/src/App.jsx
+++ fsc-task-manager-frontend/src/App.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable no-unused-vars */
 
+import Sidebar from './components/Sidebar';
 import Tasks from './components/Tasks'
+
+import './App.scss'
 
 const App = () => {
     
   return (
-    <>
+    <div className='app-container'>
+      <Sidebar />
       <Tasks />
-    </>
+    </div>
   );
 }
 
