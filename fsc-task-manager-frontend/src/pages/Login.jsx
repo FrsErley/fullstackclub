988b4acf9a4594b90a67cfa6bc326b61aaa52aa6
frsErley <frsleeh@gmail.com>
2024-02-06 12:40:56 -0300

    feat: add navigation to sign in and sign out buttons

--- fsc-task-manager-frontend/src/pages/Login.jsx
+++ fsc-task-manager-frontend/src/pages/Login.jsx
@@ -1,13 +1,21 @@
+import { useNavigate } from 'react-router-dom'
+
 import './Login.scss'
 import logo2 from '../assets/images/logo-2.png'
 import CustomButton from '../components/CustomButton'
 
 const Login = () => {
+    const navigate = useNavigate()
+
+    const handleSignInClick = () => {
+        navigate('/')
+    }
+
     return (
         <div className="login-container">
             <img src={logo2} alt="" />
             <div className="button-container">
-                <CustomButton>Entrar</CustomButton>
+                <CustomButton onClick={handleSignInClick}>Entrar</CustomButton>
             </div>
         </div>
     )
