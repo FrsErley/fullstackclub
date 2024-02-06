6cfaa17194c3ce9653f3c84d856cccebad35b1cc
frsErley <frsleeh@gmail.com>
2024-02-06 14:34:13 -0300

    chore: add configs to eslint

--- fsc-task-manager-frontend/src/index.js
+++ fsc-task-manager-frontend/src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { positions, transitions, Provider as AlertProvider } from 'react-alert'
 import alertTemplate from 'react-alert-template-basic'
-import {BrowserRouter} from'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import './index.scss';
-import reportWebVitals from './reportWebVitals';
+import './index.scss'
+import reportWebVitals from './reportWebVitals'
 
-import App from './App';
+import App from './App'
 
 const options = {
   positions: positions.BOTTOM_CENTER,
@@ -16,7 +16,7 @@ const options = {
   transition: transitions.SCALE
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -25,9 +25,9 @@ root.render(
       </AlertProvider>
     </BrowserRouter>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
